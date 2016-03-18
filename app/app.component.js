7ce44cbe61d3314c58fc0641791ae64abc14e7f3
BooM <boom41728@gmail.com>
2016-03-19 01:19:03 +0700

    #2
    Login Page

--- app/app.component.js
+++ app/app.component.js
@@ -23,8 +23,8 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 }
                 AppComponent = __decorate([
                     core_1.Component({
-                        selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        selector: 'index',
+                        templateUrl: 'app/view/login.html'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
